42a2ff40abaa3c34f7c3aa00d75c5350e415e74a
Eric Yihan Chen <ericyihc@usc.edu>
2018-03-03 13:12:45 -0800

    image emotion analysis and text_analysis (opinion 0~1) done in javascript

---
+++ text_analysis.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let https = require ('https');
+
+let accessKey = 'dc1c5662c02a47988db52293c248400d';
+
+let uri = 'westus.api.cognitive.microsoft.com';
+let path = '/text/analytics/v2.0/sentiment';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+        let body_ = JSON.parse (body);
+        let body__ = JSON.stringify (body_, null, '  ');
+        console.log (body__);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let get_sentiments = function (documents) {
+    let body = JSON.stringify (documents);
+
+    let request_params = {
+        method : 'POST',
+        hostname : uri,
+        path : path,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.write (body);
+    req.end ();
+}
+
+// here is where you put in the text and get result
+// the return type is a score forom 0~1, Meaning if it's neutrual (0.5) or has a strong opinion. Not sure if this can help a lot
+let documents = { 'documents': [
+    { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+    { 'id': '2', 'language': 'es', 'text': 'Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.' },
+]};
+
+get_sentiments (documents);
