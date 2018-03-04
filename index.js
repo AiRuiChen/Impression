74518b8552e6d7736314e8b629a18d0fb0f0f839
Marcus Tan <marcus.m.tan@gmail.com>
2018-03-03 22:48:50 -0500

    integration with images, single extract, batch extract

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
+var fs = require('fs');
 
 // global vars
 var port = 3000;
@@ -14,7 +15,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/test', function (req, res) {
-    res.sendFile('/home/marcus/sentiment/detectFaces.html');
+    res.sendFile('/home/marcus/hacktech2018/test.html');
 });
 
 var createUrl = function () {
@@ -57,20 +58,21 @@ var processImage = function (sourceImageUrl, cb) {
     });
 };
 
+var processMedia = function(post, cb) {
+    if (post.is_video) {
+        processVideo(post.shortcode)
+    } else {
+        processImage(post.display_url, function (body) {
+            cb(body);
+        });
+    }
+}
+
 // takes an array of instagram post objects [ {}, {}, ... ]
 var processAllPosts = function(posts) {
-            console.log('adsf')
-
-    // for (var k in posts) {
-    //     processImage(posts[k].display_url, function(body) {
-    //         console.log(posts[k].display_url + ': ' + body);
-    //     });
-    // }
-    // var test = 'https://scontent-lax3-1.cdninstagram.com/vp/a54f2f0075ee8d560b5ab9143b6d3e34/5B30074B/t51.2885-15/e35/28156423_402534350207448_5780298095128477696_n.jpg';
-    
-    async.eachLimit(posts, 20,
+    async.eachLimit(posts, 1,
         function(post, cb) {
-            processImage(post.display_url, function (body) {
+            processMedia(post, function(body) {
                 console.log("done");
                 console.log(body);
                 cb();
@@ -86,6 +88,22 @@ var extractData = function (data) {
     var strMatch = 'window._sharedData = ';
     var processed = [];
 
+    // { node: 
+    //  { comments_disabled: false,
+    //    id: '1727114807520537321',
+    //    edge_media_to_caption: [Object],
+    //    shortcode: 'Bf38cRMlCrp',
+    //    edge_media_to_comment: [Object],
+    //    taken_at_timestamp: 1520108150,
+    //    dimensions: [Object],
+    //    display_url: 'https://scontent-lax3-1.cdninstagram.com/vp/78b128fe8de601a75bb51ce1ce9332fe/5B2E1E7E/t51.2885-15/e35/28433283_434163273681080_1300260933221744640_n.jpg',
+    //    edge_liked_by: [Object],
+    //    edge_media_preview_like: [Object],
+    //    owner: [Object],
+    //    thumbnail_src: 'https://scontent-lax3-1.cdninstagram.com/vp/033295904bc1270b8574d2cb7541fe75/5B4097EC/t51.2885-15/s640x640/sh0.08/e35/c100.0.880.880/28433283_434163273681080_1300260933221744640_n.jpg',
+    //    thumbnail_resources: [Object],
+    //    is_video: false } },
+
     $('script').each(function(i, element){
         if (element.children.length && element.children[0].data.substring(0, strMatch.length) === strMatch) {
             var extracted = element.children[0].data;
@@ -97,60 +115,148 @@ var extractData = function (data) {
             for (var i = 0; i < tagPage.length; i++) {
                 var edges = tagPage[i].graphql.hashtag;
 
-                // { node: 
-                //  { comments_disabled: false,
-                //    id: '1727114807520537321',
-                //    edge_media_to_caption: [Object],
-                //    shortcode: 'Bf38cRMlCrp',
-                //    edge_media_to_comment: [Object],
-                //    taken_at_timestamp: 1520108150,
-                //    dimensions: [Object],
-                //    display_url: 'https://scontent-lax3-1.cdninstagram.com/vp/78b128fe8de601a75bb51ce1ce9332fe/5B2E1E7E/t51.2885-15/e35/28433283_434163273681080_1300260933221744640_n.jpg',
-                //    edge_liked_by: [Object],
-                //    edge_media_preview_like: [Object],
-                //    owner: [Object],
-                //    thumbnail_src: 'https://scontent-lax3-1.cdninstagram.com/vp/033295904bc1270b8574d2cb7541fe75/5B4097EC/t51.2885-15/s640x640/sh0.08/e35/c100.0.880.880/28433283_434163273681080_1300260933221744640_n.jpg',
-                //    thumbnail_resources: [Object],
-                //    is_video: false } },
-
                 var stack = [edges.edge_hashtag_to_top_posts.edges, edges.edge_hashtag_to_media.edges];
                 for (var n = 0; n < stack.length; n++) {            
                     var nodes = stack[n];
                     for (var j = 0; j < nodes.length; j++) {
-                        var raw_caption = (nodes[j].node.edge_media_to_caption.edges.length > 0) ? 
-                                         nodes[j].node.edge_media_to_caption.edges[0].node.text : '';
+                        var node = nodes[j].node
+                        // console.log(node);
+                        var raw_caption = (node.edge_media_to_caption.edges.length > 0) ? 
+                                         node.edge_media_to_caption.edges[0].node.text : '';
                         processed.push({
-                            display_url: nodes[j].node.display_url,
+                            shortcode: node.shortcode,
+                            display_url: node.display_url,
                             raw_caption: raw_caption,
-                            is_video: nodes[j].node.is_video
+                            is_video: node.is_video
                         });
                     }
                 }
             }
         }
     });
+
     // console.log('////////////////////////////////');
     // console.log(processed);
     return processed;
 };
 
+var getVideoUrl = function(shortcode, cb) {
+    var searchUrl = 'https://www.instagram.com/p/' + shortcode;
+    var called = false;
+    request(searchUrl, function (err, res, data) {
+        if (!err && res.statusCode === 200) {
+            var $ = cheerio.load(data);
+            $('meta').each(function(i, element){
+                if (element.attribs.property === 'og:video:secure_url') {
+                    cb(element.attribs.content);
+                    called = true;
+                }
+            });
+        }
+        if (!called) {
+            cb('');
+        }
+    });
+}
+
+var addVideoUrls = function(posts, outerCb) {
+    async.eachLimit(posts, 20,
+        function(post, cb) {
+            getVideoUrl(post.shortcode, function(video_url) {
+                // console.log(video_url);
+                post.video_url = video_url;
+                cb();
+            });
+        }, function(err) {
+            outerCb(err);
+        });
+}
+
+var extractSinglePost = function(shortcode) {
+    var searchUrl = 'https://www.instagram.com/p/' + shortcode;
+    var called = false;
+
+    request(searchUrl, function (err, res, data) {
+        if (!err && res.statusCode === 200) {
+            var $ = cheerio.load(data);
+            var strMatch = 'window._sharedData = ';
+            var processed;
+            var video_url = '';
+            
+            $('meta').each(function(i, element){
+                if (element.attribs.property === 'og:video:secure_url') {
+                    video_url = element.attribs.content;
+                    return false;
+                }
+            });
+            
+            $('script').each(function(i, element){
+                if (element.children.length && element.children[0].data.substring(0, strMatch.length) === strMatch) {
+                    var extracted = element.children[0].data;
+                    var results = JSON.parse(extracted.substring(strMatch.length, extracted.length-1));
+
+                    postPage = results.entry_data.PostPage;
+                    for (var i = 0; i < postPage.length; i++) {
+                        var node = postPage[i].graphql.shortcode_media;
+
+                        var raw_caption = (node.edge_media_to_caption.edges.length > 0) ? 
+                                         node.edge_media_to_caption.edges[0].node.text : '';
+                        processed = {
+                            shortcode: node.shortcode,
+                            display_url: node.display_url,
+                            raw_caption: raw_caption,
+                            is_video: node.is_video,
+                            video_url: video_url
+                        };
+                    }
+                }
+            });
+
+            return processed;
+
+        } else {
+            return {
+                shortcode: shortcode,
+                error: 'true'
+            }
+        }
+    });    
+}
+
+// extractSinglePost('BfjLmGPHSh-');
+// extractSinglePost('BdAuTvHhcVe')
+
 var main = function() {
     // scrape instagram
-    var searchUrl = 'https://www.instagram.com/explore/tags/beach/';
+    var searchUrl = 'https://www.instagram.com/explore/tags/soylent/';
     request(searchUrl, function (err, res, data) {
         if (!err && res.statusCode === 200) {
 
             // extract useful info from raw scrape
             var posts = extractData(data);
 
+            // populating video urls
+            addVideoUrls(posts, function(err) {
+                if (err) {
+                    console.log('ERROR')
+                    console.log(err)
+                }
+                // console.log('///////////')
+                // console.log(posts);
+                // fs.writeFile('test.json', JSON.stringify(posts, null, 4), 'utf8');
+            });
+
             console.log('finished extracting');
+            extractSinglePost('BfjLmGPHSh-');
+            
             // process each post
-            processAllPosts(posts);
+            // processAllPosts(posts);
         }
     });
 };
 
-main();
+// main();
+
 
 app.listen(port, function () {
     console.log('listening on ' + port);
