483ddea517f148266223ef87e149d8cf07244701
JunGwon <j464kim@uwaterloo.ca>
2018-03-03 23:31:11 -0800

    feed speech script into the tone analyzer

--- tone_analysis.js
+++ tone_analysis.js
@@ -8,16 +8,22 @@ var tone_analyzer = new ToneAnalyzerV3({
 	url: 'https://gateway.watsonplatform.net/tone-analyzer/api/'
 });
 
-var params = {
-	'text': 'A comma-separated list of tones for which the service is to return its analysis of the input. The indicated tones apply both to the full document and to individual sentences of the document. You can specify one or more of the following values',
-	'content_type': 'text/plain'
-};
+var get_ibm_sentiments = function () {
 
-tone_analyzer.tone(params,
-	function (error, response) {
-		if (error)
-			console.log('error:', error);
-		else
-			console.log(JSON.stringify(response, null, 2));
-	}
-);
+	speech_text = get_speech_analysis();
+
+	var speech_params = {
+		// 'text': 'A comma-separated list of tones for which the service is to return its analysis of the input. The indicated tones apply both to the full document and to individual sentences of the document. You can specify one or more of the following values',
+		'text': speech_text,
+		'content_type': 'text/plain'
+	};
+
+	tone_analyzer.tone(speech_params,
+		function (error, response) {
+			if (error)
+				console.log('error:', error);
+			else
+				console.log(JSON.stringify(response, null, 2));
+		}
+	);
+};
