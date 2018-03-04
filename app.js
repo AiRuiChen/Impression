49ab8e5ae866050ec4fffb3d4a9c2b6902053520
JunGwon <j464kim@uwaterloo.ca>
2018-03-04 03:20:38 -0800

    add media cards, select dropbox input field with dump data

--- app.js
+++ app.js
@@ -1,40 +1,110 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope, $http) {
-	$scope.submit = function () {
-		var data = $.param({
-			video: JSON.stringify({
-				title: $scope.title,
-				url: $scope.url,
-				body: $scope.body
-			})
-		});
+var app = angular.module('myApp', [
+//	Module Dependencies
+	'ngMaterial'
+]);
+app.controller('myCtrl',
+	function ($scope) {
+		$scope.imagePath = 'https://www.funnypica.com/wp-content/uploads/2015/05/Funny-Dog-faces-4.jpg';
+		$scope.searchMethods = ['# Hash Tag', '/ Short path']
+		$scope.myObj =
+			[
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				},
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				},
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				},
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				},
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				},
+				{
+					"anger": 0.037,
+					"contempt": 0.001,
+					"disgust": 0.015,
+					"fear": 0.001,
+					"happiness": 0.939,
+					"neutral": 0.001,
+					"sadness": 0.0,
+					"surprise": 0.007
+				}
 
-		// get request
-		$http({
-			method: 'GET',
-			url: '/someUrl'
-		}).then(function successCallback(response) {
-			// handle success
-			$scope.videos = response.data;
-		}, function errorCallback(response) {
-			// handle error
-			console.log(JSON.stringify("Failed to get t&c: " + response));
-		});
+			];
 
-		// post request
-		$http({
-			method: 'POST',
-			url: '/someUrl',
-			headers: {
-				'Content-Type': undefined
-			},
-			data: data,
-		}).then(function successCallback(response) {
-			// handle success here
+		$scope.submit = function () {
+			var data = $.param({
+				video: JSON.stringify({
+					title: $scope.title,
+					url: $scope.url,
+					body: $scope.body
+				})
+			});
+			// get request
+			$http({
+				method: 'GET',
+				url: '/someUrl'
+			}).then(function successCallback(response) {
+				// handle success
+				$scope.videos = response.data;
+			}, function errorCallback(response) {
+				// handle error
+				console.log(JSON.stringify("Failed to get t&c: " + response));
+			});
 
-		}, function errorCallback(response) {
-			// handle error
-			console.log(JSON.stringify("Failed to get t&c: " + response));
-		});
-	}
-});
+			// post request
+			$http({
+				method: 'POST',
+				url: '/someUrl',
+				headers: {
+					'Content-Type': undefined
+				},
+				data: data,
+			}).then(function successCallback(response) {
+				// handle success here
+
+			}, function errorCallback(response) {
+				// handle error
+				console.log(JSON.stringify("Failed to get t&c: " + response));
+			});
+		}
+	});
